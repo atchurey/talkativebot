c00fe19817d353d023c8a839523b1d4104314d5d
Albert Atchurey <albert.atchurey@tech11.com>
2026-05-30 04:30:49 +0200

    cosmetics and docu

--- talkativebot-core/src/main/java/com/atchurey/tools/talkativebot/core/channel/DefaultOptionSelector.java
+++ talkativebot-core/src/main/java/com/atchurey/tools/talkativebot/core/channel/DefaultOptionSelector.java
@@ -6,6 +6,18 @@ import com.atchurey.tools.talkativebot.core.questions.Question;
 import java.util.Arrays;
 import java.util.Optional;
 
+/**
+ * A default implementation of the {@link OptionSelector} interface.
+ * This class provides functionality for selecting an {@link Option}
+ * based on user input from an {@link IncomingMessage}. The selection 
+ * is made using either the numerical value of the option or its text.
+ *
+ * The selection logic is as follows:
+ * 1. Attempts to parse the input text as an integer and match it 
+ *    against the value of the options in the given {@link Question}.
+ * 2. If value finds no match, attempts to match the input 
+ *    text against the text of the options, ignoring case sensitivity.
+ */
 public class DefaultOptionSelector implements OptionSelector {
 
     @Override
